ac77b8565306d24ea19b4f35c4128fcdf8f9264e
vivsw <131902937+vivsw@users.noreply.github.com>
2025-07-09 09:25:26 -0400

    Update index.tsx

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -9,7 +9,7 @@ import { getMarkdownContent } from "@lib/contentHelpers";
  * This path is relative to the [root]/content/ directory. Modify this value
  * to change the directory that is used to generate the page.
  */
-const CONTENT_DIRECTORY = "team";
+const CONTENT_DIRECTORY = "contact";
 
 /**
  * This is a page that is generated from a Markdown file.
